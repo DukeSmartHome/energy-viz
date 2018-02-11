9ce6c1ea92d6ba46dee3af9fbfa5026cbf20a76c
Sujay Garlanka <sujay.garlanka@gmail.com>
2018-02-10 21:12:07 -0500

    testing time

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     while True:
         database.insertAppliances(energyData.getAppliances())
         database.insertUtilities(energyData.getUtilities())
-        time.sleep(300)
+        time.sleep(5)
