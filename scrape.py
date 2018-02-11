a34cce77a046392d17dbed7fe2f5f73bf2e2d11c
rahulgs12 <gsr1998@gmail.com>
2018-02-10 19:50:10 -0500

    class implementation

--- scrape.py
+++ scrape.py
@@ -3,17 +3,33 @@ from xmljson import badgerfish as bf
 from xml.etree.ElementTree import fromstring
 from json import dumps, loads
 
-urls = ["http://152.3.3.246/cgi-bin/egauge?noteam",
-"http://152.3.3.236/cgi-bin/egauge?noteam",
-"http://152.3.3.210/cgi-bin/egauge?noteam",
-"http://152.3.3.214/cgi-bin/egauge?noteam",
-"http://152.3.3.235/cgi-bin/egauge?noteam",
-"http://152.3.3.213/cgi-bin/egauge?noteam"] # XML URLS
+class EnergyData:
+    appliances = [
+    "http://152.3.3.210/cgi-bin/egauge?noteam",
+    "http://152.3.3.214/cgi-bin/egauge?noteam",
+    "http://152.3.3.235/cgi-bin/egauge?noteam",
+    "http://152.3.3.213/cgi-bin/egauge?noteam"]
+    utilities = ["http://152.3.3.246/cgi-bin/egauge?noteam",
+    "http://152.3.3.236/cgi-bin/egauge?noteam"]
 
-for i in urls: #
-    r = requests.get(i) # r is a request object
-    d = loads(dumps(bf.data(fromstring(r.text)), indent = 4)) # r.text is the xml, bf.data returns OrderedDicts, load+dumps converts to Dict
-    for i in d["measurements"]["meter"]:
-        print("{:<40} {:<20} {:<20}".format(i["@title"],i["energyWs"]["$"],i["power"]["$"]))
+    def getUtilities(self):
+        return self.getInfo(self.utilities)
 
-        ### sql stuff goes here ### 
+    def getAppliances(self):
+        return self.getInfo(self.appliances)
+
+    def getInfo(self, urls):
+        rt = []
+        for i in urls: #
+            r = requests.get(i) # r is a request object
+            d = loads(dumps(bf.data(fromstring(r.text)), indent = 4)) # r.text is the xml, bf.data returns OrderedDicts, load+dumps converts to Dict
+            timestamp = d["measurements"]["timestamp"]["$"]
+            for i in d["measurements"]["meter"]:
+                # print("{:<40} {:<20} {:<20}".format(i["@title"],i["energyWs"]["$"],i["power"]["$"]))
+                rt.append({"timestamp":timestamp, "source":i["@title"], "energy":i["energyWs"]["$"],"power":i["power"]["$"] })
+        return rt
+
+a = EnergyData()
+print(a.getUtilities())
+print()
+print(a.getAppliances())
