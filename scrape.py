5cefe224e0d9b50c325d78a510762134e7134e45
rahul <rgs24@duke.edu>
2018-04-08 19:13:43 -0400

    added discrete and cumulative craping functions

--- scrape.py
+++ scrape.py
@@ -13,12 +13,35 @@ class EnergyData:
         "http://152.3.3.213/cgi-bin/egauge?noteam"]
         self.utilities = ["http://152.3.3.246/cgi-bin/egauge?noteam",
         "http://152.3.3.236/cgi-bin/egauge?noteam"]
+        self.prevUtilities = dict()
+        self.prevAppliances = dict()
+
+    def subDict(self,a, b):
+        for i in range(len(a)):
+            a[i]["energy"] = a[i]["energy"] - b[i]["energy"]
+        return a
 
     def getUtilities(self):
+        if any(self.prevUtilities):
+            latest = self.getInfo(self.utilities)
+            diff = self.subDict(latest, self.prevUtilities)
+            return diff
+        self.prevUtilities = self.getInfo(self.utilities)
+        return dict()
+
+    def getCumulativeUtilities(self):
         return self.getInfo(self.utilities)
 
-    def getAppliances(self):
+    def getCumulativeAppliances(self):
         return self.getInfo(self.appliances)
+
+    def getAppliances(self):
+        if any(self.prevAppliances):
+            latest = self.getInfo(self.appliances)
+            diff = self.subDict(latest, self.prevAppliances)
+            return diff
+        self.prevAppliances = self.getInfo(self.appliances)
+        return dict()
 
     def getInfo(self, urls):
         rt = []
